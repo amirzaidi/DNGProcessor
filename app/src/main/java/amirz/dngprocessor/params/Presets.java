8b9d2a5cba875664de2fb373602545f0186e6e61
Amir Zaidi <azaidi@live.nl>
2019-02-23 22:33:23 +0100

    Reduce saturation to tune for better skin colours

--- app/src/main/java/amirz/dngprocessor/params/Presets.java
+++ app/src/main/java/amirz/dngprocessor/params/Presets.java
@@ -20,7 +20,7 @@ public class Presets {
                 DeviceMap.Device device = DeviceMap.get(modelTag == null ? "" : modelTag.toString());
                 device.neutralPointCorrection(tags, sensor.neutralColorPoint);
                 process.sharpenFactor = device.sharpenFactor(tags);
-                process.saturationCurve = new float[] { 2f, 1.75f, 1.25f };
+                process.saturationCurve = new float[] { 1.33f, 0.33f, 1.25f };
                 process.stretchPerc = device.stretchPerc(tags);
                 break;
             case Boosted:
