98d84c6fddb64a156786d2928d0084eb9eb41f73
Amir Zaidi <azaidi@live.nl>
2019-03-13 11:25:40 +0100

    Increase sharpening in low light

--- app/src/main/java/amirz/dngprocessor/gl/GLProgram.java
+++ app/src/main/java/amirz/dngprocessor/gl/GLProgram.java
@@ -273,7 +273,7 @@ public class GLProgram extends GLProgramBase {
     }
 
     public void setSharpenFactor(float sharpenFactor) {
-        sharpenFactor -= 9f * Math.hypot(sigma[0], sigma[1]);
+        sharpenFactor -= 7f * Math.hypot(sigma[0], sigma[1]);
         Log.d(TAG, "Sharpen " + sharpenFactor);
         setf("sharpenFactor", Math.max(sharpenFactor, -0.25f));
     }
