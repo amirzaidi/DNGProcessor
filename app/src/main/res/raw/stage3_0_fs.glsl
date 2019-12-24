896bb7a50161ccb816b6bac505b82160ac18f9c5
Amir Zaidi <azaidi@live.nl>
2019-12-24 23:31:39 +0100

    Use different, faster geometric method in bilateral filter

--- app/src/main/res/raw/stage3_0_fs.glsl
+++ app/src/main/res/raw/stage3_0_fs.glsl
@@ -15,35 +15,23 @@ float unscaledGaussian(float d, float s) {
 }
 
 float fr(float diffi) {
-    return unscaledGaussian(diffi, 0.5f);
+    return unscaledGaussian(diffi, 0.06f);
 }
 
 float gs(float diffx) {
-    return unscaledGaussian(diffx, 1.5f);
-}
-
-float dist2d(float x1, float y1, float x2, float y2) {
-    float dx = x2 - x1;
-    float dy = y2 - y1;
-    return sqrt(dx * dx + dy * dy);
-}
-
-float dist2d(ivec2 xy1, ivec2 xy2) {
-    return dist2d(float(xy1.x), float(xy1.y), float(xy2.x), float(xy2.y));
+    return 1.f / (diffx * diffx + 1.f);
+    //return unscaledGaussian(diffx, 0.5f);
 }
 
 float pixDiff(vec3 pix1, vec3 pix2) {
-    float dx = pix2.x - pix1.x;
-    float dy = pix2.y - pix1.y;
-    float dz = pix2.z - pix1.z;
-    return sqrt(dx * dx + dy * dy + dz * dz);
+    return length(pix2 - pix1);
 }
 
 void main() {
     ivec2 xyCenter = ivec2(gl_FragCoord.xy);
     vec3 XYZCenter = texelFetch(buf, xyCenter, 0).xyz;
 
-    int radius = 5;
+    int radius = 7;
 
     ivec2 minxy = max(ivec2(0, 0), xyCenter - radius);
     ivec2 maxxy = min(bufSize - 1, xyCenter + radius);
@@ -55,13 +43,15 @@ void main() {
         for (int x = minxy.x; x <= maxxy.x; x++) {
             ivec2 xyPixel = ivec2(x, y);
             vec3 XYZPixel = texelFetch(buf, xyPixel, 0).xyz;
+            vec2 dxy = vec2(xyPixel - xyCenter);
 
-            float scale = fr(pixDiff(XYZPixel, XYZCenter)) * gs(dist2d(xyPixel, xyCenter));
+            float scale = fr(pixDiff(XYZPixel, XYZCenter)) * gs(length(dxy));
             I += XYZPixel * scale;
             W += scale;
         }
     }
 
     result.xyz = I / W;
+    //result.xyz = XYZCenter;
     result.z = XYZCenter.z;
 }
