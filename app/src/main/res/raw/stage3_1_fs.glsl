47ca5e9e08f7358c400bd4525f7cf43791120dd8
Amir Zaidi <azaidi@live.nl>
2019-12-24 23:40:30 +0100

    Re-enable Y-value sigmoid for larger dynamic range

--- app/src/main/res/raw/stage3_1_fs.glsl
+++ app/src/main/res/raw/stage3_1_fs.glsl
@@ -239,9 +239,9 @@ vec3 processPatch(ivec2 xyPos) {
 
     //z = texture(downscaledBuffer, vec2(xyPos) / vec2(intermediateWidth, intermediateHeight)).x;
     //z = texelFetch(downscaledBuffer, xyPos / 8, 0).x;
-    //return clamp(vec3(xy, sigmoid(z, 0.25f)), 0.f, 1.f);
+    return clamp(vec3(xy, sigmoid(z, 0.25f)), 0.f, 1.f);
     //return clamp(vec3(vec2(0.345703f, 0.358539f), z), 0.f, 1.f);
-    return clamp(vec3(xy, z), 0.f, 1.f);
+    //return clamp(vec3(xy, z), 0.f, 1.f);
 }
 
 vec3 xyYtoXYZ(vec3 xyY) {
