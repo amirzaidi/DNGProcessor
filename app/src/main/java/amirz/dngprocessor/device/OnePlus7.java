302c17be6c6d73a7865c65e7f293b1aa5570bd02
Amir Zaidi <azaidi@live.nl>
2020-04-01 14:31:24 +0200

    Clean up OnePlus 7 class

--- app/src/main/java/amirz/dngprocessor/device/OnePlus7.java
+++ app/src/main/java/amirz/dngprocessor/device/OnePlus7.java
@@ -1,20 +1,11 @@
 package amirz.dngprocessor.device;
 
-import android.util.Rational;
 import android.util.SparseArray;
 
 import amirz.dngprocessor.params.SensorParams;
 import amirz.dngprocessor.parser.TIFFTag;
 
 public class OnePlus7 extends Generic {
-    private static final float[] CC1 = new float[] {
-            1f, 0f, 0.2f, // Low=Remove Pink, High=Invert Pink | Low=Remove Red, High=Cyberpunk
-            0.2f, 1f, 0.3f, // Low=Purple/Yellow, High=Magenta/Green | Low=Cyan, High=Violet/Green
-            0.1f, 0f, 1f, // | Low=Blue->Bright, High=Blue->Green
-    };
-
-    private static final float[] CC2 = CC1;
-
     @Override
     public boolean isModel(String model) {
         return model.startsWith("GM19") || model.startsWith("HD19");
@@ -22,49 +13,9 @@ public class OnePlus7 extends Generic {
 
     @Override
     public void sensorCorrection(SparseArray<TIFFTag> tags, SensorParams sensor) {
-        //sensor.calibrationTransform1 = CC1;
-        //sensor.calibrationTransform2 = CC2;
-
-        /*
-        Rational r = sensor.neutralColorPoint[0];
-        sensor.neutralColorPoint[0] = new Rational(
-                r.getNumerator() * 19,
-                r.getDenominator() * 20
-        );
-
-        Rational b = sensor.neutralColorPoint[2];
-        sensor.neutralColorPoint[2] = new Rational(
-                b.getNumerator() * 41,
-                b.getDenominator() * 40
-        );
-         */
-
         int[] bl = sensor.blackLevelPattern;
         int maxBlack = Math.max(Math.max(bl[0], bl[1]), Math.max(bl[2], bl[3]));
         sensor.blackLevelPattern = new int[] { maxBlack, maxBlack, maxBlack, maxBlack };
-
-        /*
-        sensor.colorMatrix1 = new float[] {
-                86f / 128f, -10f / 128f, -13f / 128f,
-                -52f / 128f, 167f / 128f, 9f / 128f,
-                -9f / 128f, 30f / 128f, 48f / 128f,
-        };
-        sensor.colorMatrix2 = new float[] {
-                137f / 128f, -37f / 128f, -14f / 128f,
-                -66f / 128f, 190f / 128f, 31f / 128f,
-                -4f / 128f, 14f / 128f, 81f / 128f,
-        };
-        sensor.forwardTransform1 = new float[] {
-                94f / 128f, 8f / 128f, 21f / 128f,
-                30f / 128f, 102f / 128f, -5f / 128f,
-                -1f / 128f, -45f / 128f, 152f / 128f,
-        };
-        sensor.forwardTransform2 = new float[] {
-                94f / 128f, 8f / 128f, 21f / 128f,
-                30f / 128f, 102f / 128f, -5f / 128f,
-                -1f / 128f, -45f / 128f, 152f / 128f,
-        };
-        */
 
         if (sensor.gainMap == null) {
             sensor.gainMap = new float[] {
