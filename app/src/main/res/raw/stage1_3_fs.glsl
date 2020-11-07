d85616940d4f7d43ee92566409d1d722d5fbde6f
Amir Zaidi <azaidi@live.nl>
2020-11-07 16:54:47 +0100

    Speed up demosaicing

--- app/src/main/res/raw/stage1_3_fs.glsl
+++ app/src/main/res/raw/stage1_3_fs.glsl
@@ -26,68 +26,61 @@ const int demosaicArray[16] = int[](
 // Out
 out vec3 intermediate;
 
-#include sigmoid
-#include load3x3
 #include xyztoxyy
-#include xyytoxyz
 
-int ind(int x, int y) {
-    int dim = 3;
-    return x + dim / 2 + (y + dim / 2) * dim;
+vec4 getCross(sampler2D buf, ivec2 xy) {
+    return vec4(
+        texelFetch(buf, xy + ivec2(-1, -1), 0).x,
+        texelFetch(buf, xy + ivec2(1, -1), 0).x,
+        texelFetch(buf, xy + ivec2(-1, 1), 0).x,
+        texelFetch(buf, xy + ivec2(1, 1), 0).x
+    );
+}
+
+vec2 getHorz(sampler2D buf, ivec2 xy) {
+    return vec2(
+        texelFetch(buf, xy + ivec2(-1, 0), 0).x,
+        texelFetch(buf, xy + ivec2(1, 0), 0).x
+    );
+}
+
+vec2 getVert(sampler2D buf, ivec2 xy) {
+    return vec2(
+        texelFetch(buf, xy + ivec2(0, -1), 0).x,
+        texelFetch(buf, xy + ivec2(0, 1), 0).x
+    );
+}
+
+float getScale(vec2 raw, vec2 green, float minG) {
+    return dot(raw / max(green, minG), vec2(0.5f));
+}
+
+float getScale(vec4 raw, vec4 green, float minG) {
+    return dot(raw / max(green, minG), vec4(0.25f));
 }
 
 // Apply bilinear-interpolation to demosaic
-vec3 demosaic(ivec2 xy, inout float[9] inputArray, inout float[9] greenArray) {
+vec3 demosaic(ivec2 xy) {
     int x = xy.x;
     int y = xy.y;
 
     int index = (x & 1) | ((y & 1) << 1);
     index |= (cfaPattern << 2);
     vec3 pRGB;
-    int pxType = demosaicArray[index]; -1;
-
-    /*
-    switch (index) {
-        case 0:
-        case 5:
-        case 10:
-        case 15:
-            pxType = 0; // GRG
-            break;
-        case 1:
-        case 4:
-        case 11:
-        case 14:
-            pxType = 1; // RGR
-            break;
-        case 2:
-        case 7:
-        case 8:
-        case 13:
-            pxType = 2; // BGB
-            break;
-        case 3:
-        case 6:
-        case 9:
-        case 12:
-            pxType = 3; // GBG
-            break;
-    }
-    */
+    int pxType = demosaicArray[index];
 
     // We already computed green
-    pRGB.g = greenArray[ind(0, 0)];
+    pRGB.g = texelFetch(greenBuffer, xy, 0).x;
     float minG = 0.01f;
+    float g = max(pRGB.g, minG);
 
     if (pxType == 0 || pxType == 3) {
-        float p = inputArray[ind(0, 0)];
-
-        float crosstl = inputArray[ind(-1, -1)] / max(greenArray[ind(-1, -1)], minG),
-            crosstr = inputArray[ind(1, -1)] / max(greenArray[ind(1, -1)], minG),
-            crossbl = inputArray[ind(-1, 1)] / max(greenArray[ind(-1, 1)], minG),
-            crossbr = inputArray[ind(1, 1)] / max(greenArray[ind(1, 1)], minG);
-
-        float cross = 0.25f * max(pRGB.g, minG) * (crosstl + crosstr + crossbl + crossbr);
+        float p = texelFetch(rawBuffer, xy, 0).x;
+        float cross = g * getScale(
+            getCross(rawBuffer, xy),
+            getCross(greenBuffer, xy),
+            minG
+        );
         if (pxType == 0) {
             // Red centered
             // B # B
@@ -104,14 +97,16 @@ vec3 demosaic(ivec2 xy, inout float[9] inputArray, inout float[9] greenArray) {
             pRGB.b = p;
         }
     } else if (pxType == 1 || pxType == 2) {
-        float horzl = inputArray[ind(-1, 0)] / max(greenArray[ind(-1, 0)], minG),
-            horzr = inputArray[ind(1, 0)] / max(greenArray[ind(1, 0)], minG);
-        float horz = 0.5f * max(pRGB.g, minG) * (horzl + horzr);
-
-        float vertt = inputArray[ind(0, -1)] / max(greenArray[ind(0, -1)], minG),
-            vertb = inputArray[ind(0, 1)] / max(greenArray[ind(0, 1)], minG);
-        float vert = 0.5f * max(pRGB.g, minG) * (vertt + vertb);
-
+        float horz = g * getScale(
+            getHorz(rawBuffer, xy),
+            getHorz(greenBuffer, xy),
+            minG
+        );
+        float vert = g * getScale(
+            getVert(rawBuffer, xy),
+            getVert(greenBuffer, xy),
+            minG
+        );
         if (pxType == 1) {
             // Green centered w/ horizontally adjacent Red
             // # B #
@@ -133,10 +128,8 @@ vec3 demosaic(ivec2 xy, inout float[9] inputArray, inout float[9] greenArray) {
 }
 
 
-vec3 convertSensorToIntermediate(ivec2 xy, vec3 sensor) {    // Use gainmap to increase dynamic range.
-    sensor = min(sensor, neutralPoint);
-    //return XYZtoxyY(sensorToXYZ * sensor);
-
+vec3 convertSensorToIntermediate(ivec2 xy, vec3 sensor) {
+    // Use gainmap to increase dynamic range.
     vec2 xyInterp = vec2(float(xy.x) / float(rawWidth), float(xy.y) / float(rawHeight));
     vec4 gains = texture(gainMap, xyInterp);
     vec3 neutralScaled = min(min(gains.x, gains.y), min(gains.z, gains.w)) * neutralPoint;
@@ -154,17 +147,13 @@ vec3 convertSensorToIntermediate(ivec2 xy, vec3 sensor) {    // Use gainmap to i
     vec3 XYZ = sensorToXYZ * sensor;
     vec3 intermediate = XYZtoxyY(XYZ);
 
-    intermediate.z = sigmoid(intermediate.z, 0.75f);
     return intermediate;
-    //return xyYtoXYZ(intermediate);
 }
 
 void main() {
     ivec2 xy = ivec2(gl_FragCoord.xy);
     ivec2 xyClamped = clamp(xy, ivec2(1), ivec2(rawWidth, rawHeight) - 2);
 
-    float[9] rawPatch = load3x3(xyClamped, rawBuffer);
-    float[9] greenPatch = load3x3(xyClamped, greenBuffer);
-    vec3 sensor = demosaic(xyClamped, rawPatch, greenPatch);
+    vec3 sensor = demosaic(xyClamped);
     intermediate = convertSensorToIntermediate(xy, sensor);
 }
