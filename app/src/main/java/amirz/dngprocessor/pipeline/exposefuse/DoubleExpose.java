e5571c6be4f2591da16139b5b6d754b425f8d902
Amir Zaidi <azaidi@live.nl>
2020-12-24 17:54:51 +0100

    Pool more textures

--- app/src/main/java/amirz/dngprocessor/pipeline/exposefuse/DoubleExpose.java
+++ app/src/main/java/amirz/dngprocessor/pipeline/exposefuse/DoubleExpose.java
@@ -3,6 +3,7 @@ package amirz.dngprocessor.pipeline.exposefuse;
 import amirz.dngprocessor.R;
 import amirz.dngprocessor.gl.GLPrograms;
 import amirz.dngprocessor.gl.Texture;
+import amirz.dngprocessor.gl.TexturePool;
 import amirz.dngprocessor.pipeline.Stage;
 import amirz.dngprocessor.pipeline.StagePipeline;
 import amirz.dngprocessor.pipeline.convert.EdgeMirror;
@@ -25,13 +26,13 @@ public class DoubleExpose extends Stage {
 
         Texture normalExposure = previousStages.getStage(EdgeMirror.class).getIntermediate();
 
-        mUnderexposed = new Texture(normalExposure.getWidth(), normalExposure.getHeight(),
-                1, Texture.Format.Float16, null);
+        mUnderexposed = TexturePool.get(normalExposure.getWidth(), normalExposure.getHeight(), 1,
+                Texture.Format.Float16);
         converter.setTexture("buf", normalExposure);
         converter.setf("factor", 0.67f);
         converter.drawBlocks(mUnderexposed);
 
-        mOverexposed = new Texture(mUnderexposed);
+        mOverexposed = TexturePool.get(mUnderexposed);
         converter.setTexture("buf", normalExposure);
         converter.setf("factor", 1.5f);
         converter.drawBlocks(mOverexposed);
--- app/src/main/java/amirz/dngprocessor/pipeline/exposefuse/FuseUtils.java
+++ app/src/main/java/amirz/dngprocessor/pipeline/exposefuse/FuseUtils.java
@@ -3,16 +3,17 @@ package amirz.dngprocessor.pipeline.exposefuse;
 import amirz.dngprocessor.R;
 import amirz.dngprocessor.gl.GLPrograms;
 import amirz.dngprocessor.gl.Texture;
+import amirz.dngprocessor.gl.TexturePool;
 
 public class FuseUtils {
     public static Texture downsample2x(GLPrograms converter, Texture in) {
-        Texture downsampled = new Texture(in.getWidth() / 2 + 1,
+        Texture downsampled = TexturePool.get(in.getWidth() / 2 + 1,
                 in.getHeight() / 2 + 1,
                 in.getChannels(),
-                in.getFormat(), null);
+                in.getFormat());
 
-        try (Texture tmp2 = new Texture(in)) {
-            try (Texture tmp = new Texture(in)) {
+        try (Texture tmp2 = TexturePool.get(in)) {
+            try (Texture tmp = TexturePool.get(in)) {
                 blur2x(converter, in, tmp, tmp2);
             }
 
@@ -26,13 +27,13 @@ public class FuseUtils {
     }
 
     public static Texture upsample2x(GLPrograms converter, Texture in, Texture dimens) {
-        Texture upsampled = new Texture(dimens);
+        Texture upsampled = TexturePool.get(dimens);
 
         converter.useProgram(R.raw.stage4_3_upsample);
         converter.setTexture("buf", in);
         converter.drawBlocks(upsampled);
 
-        try (Texture tmp = new Texture(upsampled)) {
+        try (Texture tmp = TexturePool.get(upsampled)) {
             blur2x(converter, upsampled, tmp, upsampled);
         }
 
